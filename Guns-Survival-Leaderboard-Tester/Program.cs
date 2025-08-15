aeb8408705bee1793dc783f72afa36bb9aa52b7b
Digital <73296205+digital8150@users.noreply.github.com>
2025-08-16 00:45:12 +0900

    feature: 실제 프로그램 엔드포인트 작성

--- Guns-Survival-Leaderboard-Tester/Program.cs
+++ Guns-Survival-Leaderboard-Tester/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+using Guns_Survival_Leaderboard_Tester.Services;
+using Guns_Survival_Leaderboard_Tester.View;
+using Guns_Survival_Leaderboard_Tester.Controller;
+using System.Globalization;
+
+namespace Guns_Survival_Leaderboard_Tester
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string baseUrl = "https://oci.codingbot.kr";
+
+            var service = new LeaderboardService(baseUrl);
+            var view = new LeaderboardView();
+            var controller = new LeaderboardController(service, view);
+
+            await controller.RunAsync();
+        }
+    }
+}
