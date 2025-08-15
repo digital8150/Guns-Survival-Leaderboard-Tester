be9907710aa595a24107ff2d25832ef07c2eaed3
Digital <73296205+digital8150@users.noreply.github.com>
2025-08-16 00:44:38 +0900

    fix: api 명세에 맞게 프로퍼티 명 수정

--- Guns-Survival-Leaderboard-Tester/Model/Score.cs
+++ Guns-Survival-Leaderboard-Tester/Model/Score.cs
@@ -10,30 +10,30 @@ namespace Guns_Survival_Leaderboard_Tester.Model
     // API 요청에 사용하는 데이터
     public class ScoreCreate
     {
-        public int Score { get; set; }
-        public int SurvivalTime { get; set; }
-        public string Nickname { get; set; }
+        public int score { get; set; }
+        public int survival_time { get; set; }
+        public string nickname { get; set; }
 
         public ScoreCreate(int score, int survivalTime, string nickname)
         {
-            Score = score;
-            SurvivalTime = survivalTime;
-            Nickname = nickname;
+            this.score = score;
+            this.survival_time = survivalTime;
+            this.nickname = nickname;
         }
     }
 
     // API 응답에 사용하는 데이터
     public class ScoreEntry
     {
-        public string Id { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public int Score { get; set; }
-        public int SurvivalTime { get; set; }
-        public string Nickname { get; set; }
+        public string id { get; set; }
+        public DateTime registration_date { get; set; }
+        public int score { get; set; }
+        public int survival_time { get; set; }
+        public string nickname { get; set; }
 
         public override string ToString()
         {
-            return $"{Nickname} - Score: {Score}, Survival Time: {SurvivalTime}, Registered: {RegistrationDate}";
+            return $"{nickname} - Score: {score}, Survival Time: {survival_time}, Registered: {registration_date}";
         }
     }
 }
